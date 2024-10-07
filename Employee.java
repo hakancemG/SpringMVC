4c480aeb28fc3d3f9fec0d21865a3f79cacbc722
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2024-10-07 08:38:50 +0300

    Employee.java
    "Employee" class.

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package com.hakancem.starter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String id;
+    private String firstName;
+    private String lastName;
+}
